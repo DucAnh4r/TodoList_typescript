f040509a989ee78ac9b94e28f0036ac24326fa2b
DucAnh4r <ducanhb8a4@gmail.com>
2025-03-06 21:23:23 +0700

    first commit

---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,45 @@
+import { Todo } from '../../@types/todo.type'
+import styles from './TaskList.module.scss'
+
+interface TaskListProps {
+  doneTaskList?: boolean
+  todos: Todo[]
+  handleDoneTodo: (id: string, done: boolean) => void
+  startEditTodo: (id: string) => void
+  deleteTodo: (id: string) => void
+}
+
+export default function TaskList(props: TaskListProps) {
+  const { doneTaskList, todos, handleDoneTodo, startEditTodo, deleteTodo } = props
+
+  const onChangeCheckbox = (idTodo: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    handleDoneTodo(idTodo, event.target.checked)
+  }
+
+  return (
+    <div className='mb-2'>
+      <h2 className={styles.title}>{doneTaskList ? 'Hoàn thành' : 'Chưa hoàn thành'}</h2>
+      <div className={styles.tasks}>
+        {todos.map((todo) => (
+          <div className={styles.task} key={todo.id}>
+            <input
+              type='checkbox'
+              className={styles.taskCheckBox}
+              checked={todo.done}
+              onChange={onChangeCheckbox(todo.id)}
+            />
+            <span className={`${styles.taskName} ${todo.done ? styles.taskNameDone : ''}`}>{todo.name}</span>
+            <div className={styles.taskActions}>
+              <button className={styles.taskBtn} onClick={() => startEditTodo(todo.id)}>
+                ✏
+              </button>
+              <button className={styles.taskBtn} onClick={() => deleteTodo(todo.id)}>
+                🗑
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
